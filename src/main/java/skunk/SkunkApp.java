39056cc03b7a3d30c1b594fb451e8ae2229b3deb
dtran6525 <duytran2310@gmail.com>
2017-07-15 16:27:50 -0500

    Added UI Logic

--- src/main/java/skunk/SkunkApp.java
+++ src/main/java/skunk/SkunkApp.java
@@ -1,5 +1,23 @@
 package main.java.skunk;
 
-public class SkunkApp {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class SkunkApp {
+	
+	public static final int INITIAL_CHIPS = 100;
+	
+	public static void main(String[] args) {
+		SkunkApp app = new SkunkApp();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("How many players?");
+		int playerNum = sc.nextInt();
+		List<Player> players = new ArrayList<>();
+		for (int i = 0 ; i < playerNum ; i++) {
+			int playerIndex = i + 1;
+			System.out.print("Enter player " + playerIndex + "'s name:");
+			players.add(new Player(sc.next(), INITIAL_CHIPS));
+		}
+	}
 }
